a24d7d7e4312d03aae76bb9a0f6def520da3370b
gvmzin <gustavomoraisgvm@gmail.com>
2025-09-11 15:31:24 -0300

    Ajusta formato da connection string e reativa migração em postgre

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 var app = builder.Build();
-// using (var scope = app.Services.CreateScope())
-// {
-//     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//     dbContext.Database.Migrate();
-// }
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 if (!app.Environment.IsDevelopment())
 {
